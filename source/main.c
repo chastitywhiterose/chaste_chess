598c4c3a235c466f5c296fcb6814358a87074f04
Chastity White Rose <chastitywhiterose@gmail.com>
2023-12-31 02:08:21 -0600

    Add files via upload

--- source/main.c
+++ source/main.c
@@ -1,1 +1,54 @@
-ddd
+#include <stdio.h>
+#include <SDL.h>
+
+int width=720,height=720;
+int loop=1;
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Event e;
+SDL_Rect rect;
+
+
+#include "sdl_chaste_checkerboard.h"
+
+
+int main(int argc, char **argv)
+{
+ if(SDL_Init(SDL_INIT_VIDEO))
+ {
+  printf( "SDL could not initialize! SDL_Error: %s\n",SDL_GetError());return -1;
+ }
+ window=SDL_CreateWindow("Chaste Chess",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width,height,SDL_WINDOW_SHOWN );
+ if(window==NULL){printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );return -1;}
+ renderer = SDL_CreateRenderer(window,-1,0);
+ if(renderer==NULL){printf( "Renderer could not be created! SDL_Error: %s\n", SDL_GetError() );return -1;}
+
+ SDL_SetRenderDrawColor(renderer,0x55,0x55,0x55,255);
+ SDL_RenderFillRect(renderer,NULL);
+
+ init_checkerboard();
+
+ main_check.rectsize=height/8;
+
+ SDL_SetRenderDrawColor(renderer,0xAA,0xAA,0xAA,255);
+ /*SDL_SetRenderDrawColor(renderer,0x80,0x80,0x80,255);*/
+ chaste_checker();
+
+ SDL_RenderPresent(renderer);
+ printf("SDL Program Compiled Correctly\n");
+ while(loop)
+ {
+  while(SDL_PollEvent(&e))
+  {
+   if(e.type == SDL_QUIT){loop=0;}
+   if(e.type == SDL_KEYUP)
+   {
+    if(e.key.keysym.sym==SDLK_ESCAPE){loop=0;}
+   }
+  }
+ }
+ SDL_DestroyRenderer(renderer);
+ SDL_DestroyWindow(window);
+ SDL_Quit();
+ return 0;
+}
