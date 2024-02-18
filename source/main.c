63a80cc156b0b67967cb4e5e5b19ce1ac3682bbe
Chastity White Rose <chastitywhiterose@gmail.com>
2024-02-18 00:32:09 -0600

    Add files via upload

--- source/main.c
+++ source/main.c
@@ -1,217 +1,158 @@
-#include <stdio.h>
-#include <SDL.h>
+/*
+SDL Check Bait
 
-int width=720,height=720;
+This program is an image viewer that makes use of "SDL_image" to load and save PNG files. Then it adds a checkerboard.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <SDL.h>
+#include <SDL_image.h>
+
+
+
+int width,height;
+
+char filename[256],text[256];
+SDL_Window *window = NULL;
+SDL_Surface *surface,*bitmap,*bitmap_temp;
 int loop=1;
-SDL_Window *window;
-SDL_Renderer *renderer;
 SDL_Event e;
 SDL_Rect rect;
 
-int x,y,x1,y1;
-int mouse_state;
+int fps=60; /*frames per second*/
+int sdl_time,sdl_time1,delay;
 
-char text[0x200],text_scale=1;
-int text_color=0xFF00FF;
-
-#include "sdl_chaste_checkerboard.h"
-#include "sdl_chaste_chess.h"
-#include "sdl_chaste_chess_font_texture.h"
-#include "sdl_chaste_chess_graphics.h"
+#include "sdl_chaste_checkerboard_surface.h"
+#include "sdl_chastefont_surface.h"
 
 int main(int argc, char **argv)
 {
-
- init_main_grid();
- /*chess_grid_print();*/
- /*return 0;*/
+ int x;
 
  if(SDL_Init(SDL_INIT_VIDEO))
  {
   printf( "SDL could not initialize! SDL_Error: %s\n",SDL_GetError());return -1;
  }
- window=SDL_CreateWindow("Chaste Chess",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width,height,SDL_WINDOW_SHOWN );
+
+ x=IMG_Init(IMG_INIT_PNG);
+ printf("IMG_Init: %d\n",x);
+
+ /*test by using hard coded bitmap file*/
+ strcpy(filename,"./bitmap/Chastity_Progress_Flag_1280x720.png");
+
+ printf("opening filename: %s\n",filename);
+
+
+
+ bitmap_temp=IMG_Load(filename);
+
+if(bitmap_temp!=NULL)
+{
+ bitmap=SDL_ConvertSurfaceFormat(bitmap_temp,SDL_PIXELFORMAT_BGRA32,0);
+ SDL_FreeSurface(bitmap_temp);
+}
+
+ else
+ {
+  printf("No bitmap was loaded. Attempting to create blank image.\n");
+  bitmap=SDL_CreateRGBSurfaceWithFormat(0,1280,720,32,SDL_PIXELFORMAT_BGRA32);
+  SDL_FillRect(bitmap,NULL,0xFF000000);
+  strcpy(filename,"Help Page");
+
+  printf("Enter file as command line to display it.\nExample:\n\n%s ./bitmap/Chastity_Progress_Flag.png\n\n",argv[0]);
+ }
+
+ if(bitmap==NULL)
+ {
+  printf( "SDL could not load main image! SDL_Error: %s\n",SDL_GetError());
+  return 0;
+ }
+
+ width=bitmap->w;
+ height=bitmap->h;
+
+ printf("bitmap loaded:\n");
+ printf("bitmap width: %d\n",width);
+ printf("bitmap height: %d\n",height);
+
+
+ window=SDL_CreateWindow(filename,SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width,height,SDL_WINDOW_SHOWN );
  if(window==NULL){printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );return -1;}
- renderer = SDL_CreateRenderer(window,-1,0);
- if(renderer==NULL){printf( "Renderer could not be created! SDL_Error: %s\n", SDL_GetError() );return -1;}
 
- printf("SDL Program Compiled Correctly\n");
+ surface = SDL_GetWindowSurface( window ); /*get surface for this window*/
 
- init_checkerboard();
- main_check.rectsize=height/8;
+ font_8=chaste_font_load("./font/FreeBASIC Font 8.bmp");
+ main_font=font_8;
 
- /*SDL_SetRenderDrawColor(renderer,0x40,0x40,0x40,255);*/
- /*SDL_SetRenderDrawColor(renderer,0x80,0x80,0x80,255);*/
- /*SDL_SetRenderDrawColor(renderer,0xC0,0xC0,0xC0,255);*/
+ SDL_BlitSurface(bitmap,NULL,surface,NULL);
 
- SDL_SetRenderDrawColor(renderer,0xAA,0xAA,0xAA,255);
- SDL_RenderFillRect(renderer,NULL);
- SDL_SetRenderDrawColor(renderer,0x55,0x55,0x55,255);
- chaste_checker();
+/* 
+if(argc==1)
+{
+ sprintf(text,"Enter file as command line to display it.\nExample:\n\n%s ./bitmap/Chastity_Progress_Flag.png",argv[0]);
 
- main_font=chaste_font_load("./font/FreeBASIC Font 8.bmp");
+ chaste_font_draw_string_scaled_color(text,64,300,3,0xFFFFFF);
+}
+*/
 
- text_scale=8;
- text_color=0x00FF00;
 
- /*sprintf(text,"Chaste\nChess");
- chaste_font_draw_string_scaled_alpha(text,100,100,text_scale);*/
+ /*chaste_font_draw_string_scaled_color("Chastity",64,300,3,0xFFFFFF);*/
 
- chess_grid_draw();
+init_checkerboard();
+/*main_check.rectcolor=0x0000FF;*/
 
- SDL_RenderPresent(renderer);
+ main_check.x_begin=0;
+ main_check.y_begin=0;
+ /*main_check.x_end=width/2;*/
+ /*main_check.y_end=height/2;*/
 
- ps.id='0'; /*the id of the selected piece is '0' at the start of the game to signal that no piece is currently selected*/
+chaste_checker_xor();
+ SDL_UpdateWindowSurface(window);
+
+ delay=1000/fps;
 
  loop=1;
  while(loop)
  {
+  sdl_time = SDL_GetTicks();
+  sdl_time1 = sdl_time+delay;
 
-  /*the gameloop always starts with polling the input including mouse clicks.*/
-  
+ SDL_BlitSurface(bitmap,NULL,surface,NULL);
+ chaste_checker_xor();
+ SDL_UpdateWindowSurface(window);
+
+ main_check.x_begin--;
+ /*main_check.y_begin--;*/
+
+ /*time loop used to slow the game down so users can see it*/
+ while(sdl_time<sdl_time1)
+ {
+  sdl_time=SDL_GetTicks();
+ }
+
+  /*test for events and only process if they exist*/
   while(SDL_PollEvent(&e))
   {
-
    if(e.type == SDL_QUIT){loop=0;}
    if(e.type == SDL_KEYUP)
    {
     if(e.key.keysym.sym==SDLK_ESCAPE){loop=0;}
-    if(e.key.keysym.sym==SDLK_r)
-    {
-     init_main_grid();
-     turn='W';
-     draw_game_scene();
-     chess_grid_draw();
-     SDL_RenderPresent(renderer);
-    }
    }
-
-
-
-
-   if(e.type == SDL_MOUSEBUTTONDOWN)
-   {
-    x=e.button.x;
-    y=e.button.y;
-
-    /*printf("Mouse clicked at position X=%d Y=%d ",x,y);*/
-
-    /*if(e.button.button==SDL_BUTTON_LEFT){printf("SDL_BUTTON_LEFT");}
-    if(e.button.button==SDL_BUTTON_RIGHT){printf("SDL_BUTTON_RIGHT");}
-    if(e.button.button==SDL_BUTTON_MIDDLE){printf("SDL_BUTTON_MIDDLE");}
-    printf("\n");*/
-
-    x=x/main_check.rectsize;
-    y=y/main_check.rectsize;
-
-    printf("Cartesian Square clicked: X=%d Y=%d\n",x,y);
-    /*printf("Chess Square clicked: %c %d\n",'A'+x,8-y);*/
-
-    p=main_grid.array[x+y*8];
-
-    p.x=x;
-    p.y=y;
-
-    /*print information on the square selected, including what piece.*/
-
-    printf("%c %d: ",'A'+x,8-y);
-
-    if(p.id=='0')
-    {
-     printf("Blank Space\n");
-    }
-    else
-    {
-
-     if(p.color=='B'){printf("Black ");} 
-     if(p.color=='W'){printf("White ");}
-
-     if(p.id=='P'){printf("Pawn");}
-     if(p.id=='R'){printf("Rook");}
-     if(p.id=='N'){printf("kNight");}
-     if(p.id=='B'){printf("Bishop");}
-     if(p.id=='Q'){printf("Queen");}
-     if(p.id=='K'){printf("King");}
-
-     printf("\n");
-    }
-    /*printf(" %c%c",p.color,p.id);*/
-	
-
-
-    
-
- 
-    /*
-	 no piece is selected. we will try to select this piece and check the moves it might make
-     now that we know which square was clicked and what piece is there
-     the major step is to highlight which squares it is possible to move it to
-    */
-    if(ps.id=='0')
-	{
-		
-	draw_game_scene();
- 
-    init_highlight();
- 
-    check_moves_of_clicked_piece();
-
-    chess_grid_draw();
-
-    SDL_RenderPresent(renderer);
-	
-	if(p.color==turn)
-	{
-		ps=p;
-		printf("It is %c turn. This piece has been selected for possible move\n", turn );
-	}
-	
-	if(p.color!=turn)
-	{
-		ps.id='0'; /*deselect piece*/
-		printf("It is not %c turn. This piece cannot be selected for move\n", p.color );
-	}
-	
-	}
-	
-    /*
-	 piece has been selected previously
-	 move it there if it has been highlighted as valid move!
-	*/
-	else
-	{
-	 if(highlight[x+y*8]==1)
-	 {
-	  main_grid.array[x+y*8]=ps; /*move piece to new square*/
-      main_grid.array[ps.x+ps.y*8].id='0'; /*clear previous square*/
-
-      if(turn=='W'){turn='B';}
-      else{turn='W';}
-
-    	 printf("Piece moved to %d,%d\n", x,y );
-	  ps.id='0'; /*deselect piece after move*/
-	 }
-	 else
-	 {
-		 ps.id='0';
-		 printf("piece can't move there\n");
-	 }
-	 
-     draw_game_scene();
-     chess_grid_draw();
-	 
-     SDL_RenderPresent(renderer);
-
-	}
-
-   }
-
   }
-
-
  }
- SDL_DestroyRenderer(renderer);
+
+ if(0){IMG_SavePNG(surface,"output.png");}
+
+ /*free memory of bitmaps now that we are done with them*/
+ SDL_FreeSurface(font_8.surface);
+ SDL_FreeSurface(bitmap);
  SDL_DestroyWindow(window);
+ IMG_Quit();
  SDL_Quit();
+
  return 0;
 }
+
